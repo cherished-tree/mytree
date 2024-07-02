f69fcee897254437c4e74afc0a1e1e5a210953f1
cherished-tree <cregoon33@naver.com>
2024-07-02 11:40:43 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -32,7 +32,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
+        new 철규의 꿈동산.ScrollSpy(document.body, {
             target: '#mainNav',
             rootMargin: '0px 0px -40%',
         });
